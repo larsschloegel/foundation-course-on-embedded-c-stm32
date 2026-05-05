0a0595dbb47c3ee51d8c497d562158108a1a8ccb
lschloegel <lars.schloegel@gmail.com>
2026-05-05 07:37:02 +0200

    36: declaration vs definition

---
+++ Offline/36_declaration_vs_definition/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// extern Verweis auf Variablen in anderen Dateien > declaration
+int main()
+{
+    int myExamScore;
+
+    myExamScore = 540;
+
+    printf("Hello World");
+
+    return 0;
+}
